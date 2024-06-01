beaff19a82723798f9f7c568be9ed7932fffc000
Yan Avlis <ccyan05@alu.ufc.br>
2024-06-01 15:19:15 -0300

    src folder.

---
+++ src/test/java/com/codeyan/WealthWave/WealthWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeyan.WealthWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
