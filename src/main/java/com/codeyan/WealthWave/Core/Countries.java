cf2c15a69983c9a2bc0da69278a740ba82f02ef5
Yan Avlis <ccyan05@alu.ufc.br>
2024-06-05 10:16:43 -0300

    Countries entity.

---
+++ src/main/java/com/codeyan/WealthWave/Core/Countries.java
@@ -0,0 +1,22 @@
+package com.codeyan.WealthWave.Core;
+
+import java.io.Serializable;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "countries")
+@Getter
+@Setter
+public class Countries implements Serializable {
+    private static long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+   
+    @Column(length = 100, nullable = false)
+    private String name;
+}
