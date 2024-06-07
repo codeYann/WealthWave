268b3c29f65fc16c7926e5d29f6ea2740a83301f
Yan Avlis <ccyan05@alu.ufc.br>
2024-06-07 10:59:44 -0300

    Creating individual account entity

---
+++ src/main/java/com/codeyan/WealthWave/Core/IndividualAccount.java
@@ -0,0 +1,49 @@
+package com.codeyan.WealthWave.Core;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "individual_account")
+@Getter
+@Setter
+public class IndividualAccount implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(unique = true, nullable = false)
+  private UUID publicId;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private AccountType accountType;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private AccountStatus status;
+
+  @Column(length = 11, nullable = false)
+  private String cpf;
+
+  @OneToOne
+  @JoinColumn(unique = true, name = "wallet_id")
+  private Wallet wallet;
+
+  @OneToOne
+  @JoinColumn(unique = true, name = "user_id")
+  private Users user;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private Date createdAt;
+
+  @Column(name = "updated_at")
+  private Date updatedAt;
+}
