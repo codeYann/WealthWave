a7940a39f60c289bcd5165106c8fcd71c2248e45
Yan Avlis <ccyan05@alu.ufc.br>
2024-06-12 14:18:59 -0300

    Adding wallet business logic.

--- src/main/java/com/codeyan/WealthWave/core/entities/wallet/Wallet.java
+++ src/main/java/com/codeyan/WealthWave/core/entities/wallet/Wallet.java
@@ -1,10 +1,11 @@
-package com.codeyan.WealthWave.Core.entities.wallet;
+package com.codeyan.WealthWave.core.entities.wallet;
 
-import com.codeyan.WealthWave.Core.entities.account.AccountStatus;
-import com.codeyan.WealthWave.Core.entities.account.BusinessAccount;
-import com.codeyan.WealthWave.Core.entities.account.IndividualAccount;
-import com.codeyan.WealthWave.Core.entities.transaction.CreditTransaction;
-import com.codeyan.WealthWave.Core.entities.transaction.DebitTransaction;
+import com.codeyan.WealthWave.core.entities.account.AccountStatus;
+import com.codeyan.WealthWave.core.entities.account.BusinessAccount;
+import com.codeyan.WealthWave.core.entities.account.IndividualAccount;
+import com.codeyan.WealthWave.core.entities.transaction.CreditTransaction;
+import com.codeyan.WealthWave.core.entities.transaction.DebitTransaction;
+import com.codeyan.WealthWave.core.exceptions.InvalidWalletDataException;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -16,7 +17,6 @@ import org.hibernate.annotations.CreationTimestamp;
 @Entity
 @Table(name = "wallet")
 @Getter
-@Setter
 public class Wallet implements Serializable {
   private static final long serialVersionUID = 1L;
 
@@ -37,14 +37,17 @@ public class Wallet implements Serializable {
   @Column(nullable = false)
   private AccountStatus status;
 
+  @Setter
   @CreationTimestamp
   @Column(name = "last_transaction_at")
   private Date lastTransactionAt;
 
+  @Setter
   @OneToOne
   @JoinColumn(unique = true, name = "business_account_id")
   private BusinessAccount businessAccount;
 
+  @Setter
   @OneToOne
   @JoinColumn(unique = true, name = "individual_account_id")
   private IndividualAccount individualAccount;
@@ -53,12 +56,60 @@ public class Wallet implements Serializable {
   @Column(name = "created_at", nullable = false, updatable = false)
   private Date createdAt;
 
+  @Setter
   @OneToMany(mappedBy = "wallet")
   private List<DebitTransaction> debitTransactions;
 
+  @Setter
   @OneToMany(mappedBy = "wallet")
   private List<CreditTransaction> creditTransactions;
 
   @Column(name = "updated_at")
   private Date updatedAt;
+
+  public Wallet() {
+
+  }
+
+  public Wallet(BigDecimal balanceCents, BigDecimal creditLimitCents) {
+    setBalanceCents(balanceCents);
+    setCreditLimitCents(creditLimitCents);
+    setCurrency("BRL");
+    setStatus(AccountStatus.ACTIVE);
+    setLastTransactionAt(null);
+    setBusinessAccount(null);
+    setIndividualAccount(null);
+    setDebitTransactions(null);
+    setCreditTransactions(null);
+  }
+
+  public void setBalanceCents(BigDecimal balanceCents) {
+    if (balanceCents == null || balanceCents.compareTo(BigDecimal.ZERO) < 0) {
+      throw new InvalidWalletDataException("balance can't be less than zero");
+    }
+    this.balanceCents = balanceCents;
+  }
+
+  public void setCreditLimitCents(BigDecimal creditLimitCents) {
+    if (creditLimitCents == null || creditLimitCents.compareTo(BigDecimal.ZERO) < 0) {
+      throw new InvalidWalletDataException("credit limit can not be null or negative");
+    }
+    this.creditLimitCents = creditLimitCents;
+  }
+
+  public void setCurrency(String currency) {
+    if (currency == null || currency.trim().isEmpty()) {
+      throw new InvalidWalletDataException("currency can't be null or empty");
+    }
+    this.currency = currency;
+  }
+
+  public void setStatus(AccountStatus status) {
+    if (status == null) {
+      throw new InvalidWalletDataException("status can't be null");
+    }
+
+    this.status = status;
+  }
+
 }
